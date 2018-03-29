3527f7ddab46b52eb6ca0b57b29053143b01da70
Brian Addicks <brian@addicks.us>
2018-03-29 15:06:24 -0400

    added complete-sqlcommand for transaction commands

---
+++ SqlTools/Public/Complete-SqlCommand.ps1
@@ -0,0 +1,32 @@
+function Complete-SqlCommand {
+    <#
+	.SYNOPSIS
+		Completes or Commits an SqlCommand to an SqlTransaction.
+		
+    .DESCRIPTION
+        Completes or Commits an SqlCommand to an SqlTransaction.
+
+    .EXAMPLE
+        
+    .PARAMETER SqlCommand
+    SqlCommand Object to add Parameter to.
+
+	#>
+	[cmdletbinding()]
+
+	Param (
+        [Parameter(Mandatory=$true,ValueFromPipeline=$True,Position=0)]
+        [System.Data.SqlClient.SqlCommand]$SqlCommand
+    )
+
+    BEGIN {
+        
+
+    }
+
+    PROCESS {
+        # At some point we'll need to handle other types of queries, but for now I know we have
+        # INSERT and UPDATE
+        $SqlCommand.ExecuteNonQuery()
+    }
+}
