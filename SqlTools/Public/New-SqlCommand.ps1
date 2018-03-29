2b55151ce789cbf82ebf8f8f3b33fb4635542f62
Brian Addicks <brian@addicks.us>
2018-03-29 15:08:11 -0400

    added some help

--- SqlTools/Public/New-SqlCommand.ps1
+++ SqlTools/Public/New-SqlCommand.ps1
@@ -7,8 +7,9 @@ function New-SqlCommand {
         Creates a Command to execute against an existing SQL Connection.
 
     .EXAMPLE
-        Connect-SqlServer -Server "mysqlserver.example.com" -Database "MyFavoriteDb" -Credential (Get-Credential) | New-SqlCommand -SqlCommand "SELECT * FROM sys.databases"
-
+        $Connection = Connect-SqlServer -Server "mysqlserver.example.com" -Database "MyFavoriteDb" -Credential (Get-Credential)
+        $Command = $Connection | New-SqlCommand -SqlCommand "SELECT * FROM sys.databases"
+        
         Selects * from sys.databases in MyFavoriteDb on mysqlserver.example.com
 
     .PARAMETER SqlCommand
